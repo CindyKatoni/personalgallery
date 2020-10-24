d54460b66e545ff858608e6c1dea2eab0dc05ecc
CindyKatoni <katmuema@gmail.com>
2020-10-24 18:43:18 +0300

    create superuser

--- gallery/settings.py
+++ gallery/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import cloudinary
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -124,3 +125,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+cloudinary.config(
+  cloud_name = os.environ.get('CLOUDINARY_CLOUD_NAME'),  
+  api_key = os.environ.get('CLOUDINARY_API_KEY'),  
+  api_secret = os.environ.get('CLOUDINARY_API_SECRET')  
+)
--- photos/models.py
+++ photos/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
 class Photos(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
-    photo = models.FilePathField(path="/img")
+    image = CloudinaryField('image')
+    
